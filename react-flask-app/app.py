4b9a4ae9ebc1813803b925ae13017b8a97f9abcc
T0raT <tigerliu@bu.edu>
2023-04-20 16:16:47 -0400

    additional files I forgot to track

---
+++ react-flask-app/app.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, url_for, session, redirect, render_template
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
+import time
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+load_dotenv()
+
+# NEVER push the actual key to GitHub, create and keep in .env file
+# Create a .env if you don't have one already
+# I also have no idea what this session cookie stuff actually do[Tiger]
+app.secret_key = os.getenv("secret_key")
+app.config['SESSION_COOKIE_NAME'] = os.getenv('secret_config')
+TOKEN_INFO = "token_info"
+
+
+@app.route('/')
+def login():
+    return render_template('landingpage.html')
+
+@app.route('/auth')
+def auth():
+    sp_oauth = create_spotify_oauth()
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
+
+@app.route('/redirect')
+def redirectSite():
+    sp_oauth = create_spotify_oauth()
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session[TOKEN_INFO] = token_info
+    return redirect(url_for('getTracks', external=True))
+
+
+@app.route('/getTracks')
+def getTracks():
+    try:
+        token_info = get_token()
+    except:
+        print("user not logged in")
+        redirect(url_for("login", _external=False))
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    # Get top 30 tracks for the past month
+    top_tracks = sp.current_user_top_tracks(limit=30, offset=0, time_range='short_term')['items']
+    # Get top 30 artists for the past month
+    top_artists = sp.current_user_top_artists(limit=30, offset=0, time_range='short_term')['items']
+    return render_template('top_tracks_and_artists.html', top_tracks=top_tracks, top_artists=top_artists)
+
+
+def get_token():
+    token_info = session.get(TOKEN_INFO, None)
+    if not token_info:
+        raise "exception"
+    now = int(time.time())
+    is_expired = token_info['expires_at'] - now < 60
+    if (is_expired):
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+    return token_info
+
+
+# Id and secret in .env file, create one if you dont have one
+def create_spotify_oauth():
+    return SpotifyOAuth(
+        client_id=os.getenv("client_id"),
+        client_secret=os.getenv("client_secret"),
+        redirect_uri=url_for('redirectSite', _external=True),
+        scope="user-top-read"
+        # Scope for top tracks, unsure how to use multiple scopes [Tiger]
+    )
